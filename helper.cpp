6ac7cb0abb69dec9675ce6202604fce67ce30c47
Кот Красивый <catinwarmhands@users.noreply.github.com>
2016-12-12 16:58:42 +0300

    Update helper.cpp

--- helper.cpp
+++ helper.cpp
@@ -6,7 +6,8 @@
 #include <algorithm>
 using namespace std;
 
-#define XML_HAT "<?xml version=\"1.0\" encoding=\"utf-8\" ?><sql> <datatypes db=\"mysql\"> <group label=\"Numeric\" color=\"rgb(238,238,170)\"> <type label=\"Integer\" length=\"0\" sql=\"INTEGER\" quote=\"\"/> <type label=\"TINYINT\" length=\"0\" sql=\"TINYINT\" quote=\"\"/> <type label=\"SMALLINT\" length=\"0\" sql=\"SMALLINT\" quote=\"\"/> <type label=\"MEDIUMINT\" length=\"0\" sql=\"MEDIUMINT\" quote=\"\"/> <type label=\"INT\" length=\"0\" sql=\"INT\" quote=\"\"/> <type label=\"BIGINT\" length=\"0\" sql=\"BIGINT\" quote=\"\"/> <type label=\"Decimal\" length=\"1\" sql=\"DECIMAL\" re=\"DEC\" quote=\"\"/> <type label=\"Single precision\" length=\"0\" sql=\"FLOAT\" quote=\"\"/> <type label=\"Double precision\" length=\"0\" sql=\"DOUBLE\" re=\"DOUBLE\" quote=\"\"/> </group> <group label=\"Character\" color=\"rgb(255,200,200)\"> <type label=\"Char\" length=\"1\" sql=\"CHAR\" quote=\"'\"/> <type label=\"Varchar\" length=\"1\" sql=\"VARCHAR\" quote=\"'\"/> <type label=\"Text\" length=\"0\" sql=\"MEDIUMTEXT\" re=\"TEXT\" quote=\"'\"/> <type label=\"Binary\" length=\"1\" sql=\"BINARY\" quote=\"'\"/> <type label=\"Varbinary\" length=\"1\" sql=\"VARBINARY\" quote=\"'\"/> <type label=\"BLOB\" length=\"0\" sql=\"BLOB\" re=\"BLOB\" quote=\"'\"/> </group> <group label=\"Date &amp; Time\" color=\"rgb(200,255,200)\"> <type label=\"Date\" length=\"0\" sql=\"DATE\" quote=\"'\"/> <type label=\"Time\" length=\"0\" sql=\"TIME\" quote=\"'\"/> <type label=\"Datetime\" length=\"0\" sql=\"DATETIME\" quote=\"'\"/> <type label=\"Year\" length=\"0\" sql=\"YEAR\" quote=\"\"/> <type label=\"Timestamp\" length=\"0\" sql=\"TIMESTAMP\" quote=\"'\"/> </group> <group label=\"Miscellaneous\" color=\"rgb(200,200,255)\"> <type label=\"ENUM\" length=\"1\" sql=\"ENUM\" quote=\"\"/> <type label=\"SET\" length=\"1\" sql=\"SET\" quote=\"\"/> <type label=\"Bit\" length=\"0\" sql=\"bit\" quote=\"\"/> </group> </datatypes>\n"
+#define XML_HEADER "<?xml version=\"1.0\" encoding=\"utf-8\" ?><sql> <datatypes db=\"mysql\"> <group label=\"Numeric\" color=\"rgb(238,238,170)\"> <type label=\"Integer\" length=\"0\" sql=\"INTEGER\" quote=\"\"/> <type label=\"TINYINT\" length=\"0\" sql=\"TINYINT\" quote=\"\"/> <type label=\"SMALLINT\" length=\"0\" sql=\"SMALLINT\" quote=\"\"/> <type label=\"MEDIUMINT\" length=\"0\" sql=\"MEDIUMINT\" quote=\"\"/> <type label=\"INT\" length=\"0\" sql=\"INT\" quote=\"\"/> <type label=\"BIGINT\" length=\"0\" sql=\"BIGINT\" quote=\"\"/> <type label=\"Decimal\" length=\"1\" sql=\"DECIMAL\" re=\"DEC\" quote=\"\"/> <type label=\"Single precision\" length=\"0\" sql=\"FLOAT\" quote=\"\"/> <type label=\"Double precision\" length=\"0\" sql=\"DOUBLE\" re=\"DOUBLE\" quote=\"\"/> </group> <group label=\"Character\" color=\"rgb(255,200,200)\"> <type label=\"Char\" length=\"1\" sql=\"CHAR\" quote=\"'\"/> <type label=\"Varchar\" length=\"1\" sql=\"VARCHAR\" quote=\"'\"/> <type label=\"Text\" length=\"0\" sql=\"MEDIUMTEXT\" re=\"TEXT\" quote=\"'\"/> <type label=\"Binary\" length=\"1\" sql=\"BINARY\" quote=\"'\"/> <type label=\"Varbinary\" length=\"1\" sql=\"VARBINARY\" quote=\"'\"/> <type label=\"BLOB\" length=\"0\" sql=\"BLOB\" re=\"BLOB\" quote=\"'\"/> </group> <group label=\"Date &amp; Time\" color=\"rgb(200,255,200)\"> <type label=\"Date\" length=\"0\" sql=\"DATE\" quote=\"'\"/> <type label=\"Time\" length=\"0\" sql=\"TIME\" quote=\"'\"/> <type label=\"Datetime\" length=\"0\" sql=\"DATETIME\" quote=\"'\"/> <type label=\"Year\" length=\"0\" sql=\"YEAR\" quote=\"\"/> <type label=\"Timestamp\" length=\"0\" sql=\"TIMESTAMP\" quote=\"'\"/> </group> <group label=\"Miscellaneous\" color=\"rgb(200,200,255)\"> <type label=\"ENUM\" length=\"1\" sql=\"ENUM\" quote=\"\"/> <type label=\"SET\" length=\"1\" sql=\"SET\" quote=\"\"/> <type label=\"Bit\" length=\"0\" sql=\"bit\" quote=\"\"/> </group> </datatypes>\n"
+#define XML_FOOTER "</sql>"
 
 string toupper(string str) {
 	for(char& c : str) c = toupper(c);
@@ -38,9 +39,11 @@ struct Field {
 	bool isPk() {
 		return title.substr(0, 3) == "pk_";
 	}
+	
 	bool isFk() {
 		return title.substr(0, 3) == "fk_";
 	}
+	
 	string getPureTitle() {
 		if(isPk() || isFk()) return title.substr(3);
 		return title;
@@ -121,6 +124,7 @@ ostream& operator<<(ostream& os, Table table) {
 
 		os << "\n\n\tforeign key (" << field.title << ") references " << ref->title << "(" << ref->getPk().title << ")\n"
 		      "\t\ton update cascade on delete restrict";
+		
 		if (i != table.fields.size()-1)
 			os << ",";
 	}
@@ -129,13 +133,13 @@ ostream& operator<<(ostream& os, Table table) {
 }
 
 void init_refs(vector<Table>& tables) {
-	for(Table& table : tables) {
-		for (Field& field : table.fields){
+	for (Table& table : tables) {
+		for (Field& field : table.fields) {
 			if (field.isFk()) {
 				string pk_title = "pk_" + field.getPureTitle();
 
 				auto it =  find_if(tables.begin(),             tables.end(), [&](Table& table){
-					return find_if(table.fields.begin(), table.fields.end(), [&](Field& field){return field.title == pk_title;}) != table.fields.end();
+				    return find_if(table.fields.begin(), table.fields.end(), [&](Field& field){return field.title == pk_title;}) != table.fields.end();
 				});
 
 				field.ref = (it != tables.end()) ? (&(*it)) : (nullptr);
@@ -159,12 +163,12 @@ string toXML(Table& table) {
 	result << "<table x=\"500\" y=\"200\" name=\""<< table.title << "\">\n";
 
 	for (Field& field : table.fields) {
-		int null = field.info.find("not null") == string::npos;
-		int autoincrement = field.isPk();
+		int isNull = (field.info.find("not null") == string::npos);
+		int isAutoincrement = field.isPk();
 
 		result << "\t<row name=\"" << field.title << "\" "
-		          "null=\"" << null << "\" "
-		          "autoincrement=\"" << autoincrement << "\">\n";
+		          "null=\"" << isNull << "\" "
+		          "autoincrement=\"" << isAutoincrement << "\">\n";
 		
 		result << "\t\t<datatype>" << toupper(field.type) << "</datatype>\n";
 		result << "\t\t<default>NULL</default>\n";
@@ -210,22 +214,21 @@ int main(int argc, char *argv[]) {
 	ofstream outsql("output.sql");
 	cout << "Writing to output.sql\n";
 	for(Table& table : tables) {
-		//cout   << table << '\n';
+		//cout << table << '\n';
 		outsql << table << '\n';
 	}
 
 	ofstream outxml("output.xml");
 	cout << "Writing to output.xml\n";
 
-	outxml << XML_HAT;
+	outxml << XML_HEADER;
 
 	for(Table& table : tables) {
 		string temp = toXML(table);
-		//cout   << temp << '\n';
+		//cout << temp << '\n';
 		outxml << temp << '\n';
 	}
 
-	outxml << "</sql>";
-
+	outxml << XML_FOOTER;
 	cout << "Done!\n";
 }
