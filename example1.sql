29246a165e3336c2010db296866677dcbc364d26
Кот Красивый <catinwarmhands@users.noreply.github.com>
2016-12-12 16:40:06 +0300

    Update example1.sql

--- example1.sql
+++ example1.sql
@@ -1,87 +1,50 @@
 create table sides(
 	pk_side integer,
-	title varchar(50) not null,
-
-	primary key (pk_side)
+	title varchar(50) not null
 );
 
 create table countries(
 	pk_country integer,
-	title varchar(50) not null,
-
-	primary key (pk_country)
+	title varchar(50) not null
 );
 
 create table cities(
 	pk_city integer,
 	title varchar(50) not null,
-	fk_country integer not null,
-
-	primary key (pk_city),
-
-	foreign key (fk_country) references countries(pk_country)
-		on update cascade on delete restrict,
+	fk_country integer not null
 );
 
 create table streets(
 	pk_street integer,
 	title varchar(50) not null,
-	fk_city integer not null,
-
-	primary key (pk_street),
-
-	foreign key (fk_city) references cities(pk_city)
-		on update cascade on delete restrict,
+	fk_city integer not null
 );
 
 create table addresses(
 	pk_address integer,
 	fk_street integer not null,
 	house_no varchar(50) not null,
-	apartment_no varchar(50),
-
-	primary key (pk_address),
-
-	foreign key (fk_street) references streets(pk_street)
-		on update cascade on delete restrict,
+	apartment_no varchar(50)
 );
 
 create table ufms(
 	pk_ufms integer,
 	no integer not null,
-	fk_address integer not null,
-
-	primary key (pk_ufms),
-
-	foreign key (fk_address) references addresses(pk_address)
-		on update cascade on delete restrict,
+	fk_address integer not null
 );
 
 create table gangs(
 	pk_gang integer,
 	title varchar(50) not null,
 	fk_side integer not null,
-	fk_hq integer,
-
-	primary key (pk_gang),
-
-	foreign key (fk_side) references sides(pk_side)
-		on update cascade on delete restrict,
-
-	foreign key (fk_hq) references UNFOUND(pk_UNFOUND)
-		on update cascade on delete restrict,
+	fk_hq integer
 );
 
 create table passports(
 	pk_passport integer,
 	no integer not null,
 	fk_ufms integer not null,
-	open_date date not null,
-
-	primary key (pk_passport),
-
-	foreign key (fk_ufms) references ufms(pk_ufms)
-		on update cascade on delete restrict,
+	open_date date not null
 );
 
 create table mutants(
@@ -92,77 +55,39 @@ create table mutants(
 	birth_date date not null,
 	nickname varchar(50),
 	fk_gang integer,
-	fk_passport integer,
-
-	primary key (pk_mutant),
-
-	foreign key (fk_gang) references gangs(pk_gang)
-		on update cascade on delete restrict,
-
-	foreign key (fk_passport) references passports(pk_passport)
-		on update cascade on delete restrict,
+	fk_passport integer
 );
 
 create table activity_types(
 	pk_activity_type integer,
-	title varchar(50) not null,
-
-	primary key (pk_activity_type)
+	title varchar(50) not null
 );
 
 create table activities(
 	pk_activity integer,
 	fk_activity_type integer not null,
-	activity_date date not null,
-
-	primary key (pk_activity),
-
-	foreign key (fk_activity_type) references activity_types(pk_activity_type)
-		on update cascade on delete restrict,
+	activity_date date not null
 );
 
 create table activity_members(
 	pk_activity_member integer,
 	fk_activity integer not null,
-	fk_mutant integer not null,
-
-	primary key (pk_activity_member),
-
-	foreign key (fk_activity) references activities(pk_activity)
-		on update cascade on delete restrict,
-
-	foreign key (fk_mutant) references mutants(pk_mutant)
-		on update cascade on delete restrict,
+	fk_mutant integer not null
 );
 
 create table power_causes(
 	pk_power_cause integer,
-	title varchar(50) not null,
-
-	primary key (pk_power_cause)
+	title varchar(50) not null
 );
 
 create table powers(
 	pk_power integer,
 	title varchar(50) not null,
-	fk_power_cause integer not null,
-
-	primary key (pk_power),
-
-	foreign key (fk_power_cause) references power_causes(pk_power_cause)
-		on update cascade on delete restrict,
+	fk_power_cause integer not null
 );
 
 create table mutant_powers(
 	pk_mutant_power integer,
 	fk_mutant integer not null,
-	fk_power integer not null,
-
-	primary key (pk_mutant_power),
-
-	foreign key (fk_mutant) references mutants(pk_mutant)
-		on update cascade on delete restrict,
-
-	foreign key (fk_power) references powers(pk_power)
-		on update cascade on delete restrict,
+	fk_power integer not null
 );
